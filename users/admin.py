f69135fcf5b6546dda7ad33bd19a9d94f83bf43f
Mohammad Jayeed <thisisjayeed@gmail.com>
2022-06-26 16:22:30 +0600

    updated admin panel user creation layout

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,42 @@
 from django.contrib import admin
-# from .models import NewUser
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 # # Register your models here.
-# admin.site.register(NewUser)
+
+
+
+class UserAdminConfig(UserAdmin):
+    # model = User
+    
+    search_fields = ('email','user_name')
+    ordering = ('start_date',)
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    # list_filter = ('email','user_name','is_active','is_staff')
+    list_display = ('email','user_name','is_active','is_staff')
+
+    fieldsets = (
+        (None,{'fields':('email','user_name',)}),
+        ('Permissions',{'fields':('is_active','is_staff',)}),
+        
+
+    )
+
+    # formfield_overrides = {
+    #     User.<if you want to imply on a certain field> : {'widget': Textarea(attrs={'rows':10,'cols':40})},
+
+    # }
+
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None,{
+        'classes':('wide',),
+        'fields':('email','user_name','password1','password2','is_staff','is_active'),
+        }),
+
+    )
+
+admin.site.register(User, UserAdminConfig)
